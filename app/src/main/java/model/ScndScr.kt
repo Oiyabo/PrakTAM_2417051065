32b81c02bf313344a7040afc2d85103d6b7e7c20
Oiyabo <maulanarm148@gmail.com>
2026-02-27 16:28:16 +0700

    Prak 3

--- app/src/main/java/model/ScndScr.kt
+++ app/src/main/java/model/ScndScr.kt
@@ -4,9 +4,9 @@ import com.example.praktam_2417051065.R
 
 object ScndScr {
     val dummyDate = listOf(
-        FirstScr (nama = "Kondangan", deskripsi = "Sodara nikahan", tanggal = 15, imageRes = R.drawable.BlueCal),
-        FirstScr (nama = "Ulang tahun", deskripsi = "Adek ulang tahun", tanggal = 3, imageRes = R.drawable.BlueTimeCal),
-        FirstScr (nama = "Ujian", deskripsi = "Ujian datang. tidak!!!", tanggal = 25, imageRes = R.drawable.RedCal),
-        FirstScr (nama = "Kerja Kelompok", deskripsi = "Tempatnya jaoooh", tanggal = 7, imageRes = R.drawable.RedTimeCal),
+        FirstScr (nama = "Kondangan", deskripsi = "Sodara nikahan", tanggal = 15, imageRes = R.drawable.bluecal),
+        FirstScr (nama = "Ulang tahun", deskripsi = "Adek ulang tahun", tanggal = 3, imageRes = R.drawable.bluetimecal),
+        FirstScr (nama = "Ujian", deskripsi = "Ujian datang. tidak!!!", tanggal = 25, imageRes = R.drawable.redcal),
+        FirstScr (nama = "Kerja Kelompok", deskripsi = "Tempatnya jaoooh", tanggal = 7, imageRes = R.drawable.redtimecal),
     )
 }
